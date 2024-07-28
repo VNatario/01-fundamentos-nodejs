82462b7b3e79e4085480b0c4896047c35f39564c
Vitor Natario <natariovitor@gmail.com>
2024-07-28 14:49:22 -0300

    feat: add status code

--- src/server.js
+++ src/server.js
@@ -23,10 +23,10 @@ const server = http.createServer((request, response) => {
       name: "Fulano de Tal",
       email: "fulanodetal@email.com",
     });
-    return response.end("Criação de usuário");
+    return response.writeHead(201).end("");
   }
 
-  return response.end("Hello World");
+  return response.writeHead(404).end("Not Found... :(");
 });
 
 server.listen(3333);
