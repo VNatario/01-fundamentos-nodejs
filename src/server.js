9aaa087315d7f74859dd79cb17184858d8a003b4
Vitor Natario <natariovitor@gmail.com>
2024-07-28 12:24:12 -0300

    feat: add GET and POST in  users

--- src/server.js
+++ src/server.js
@@ -1,6 +1,22 @@
 import http from "node:http";
 
+// GET -> Busca um recurso no back-end
+// POST -> Cria um recurso no back-end
+// PUT -> Atualiza um recurso no back-end
+// PATCH -> Atualiza uma informação específica de um recurso no back-end
+// DELETE -> Deleta um recurso no back-end
+
 const server = http.createServer((request, response) => {
+  const { method, url } = request;
+
+  if (method === "GET" && url === "/users") {
+    return response.end("Listagem de usuários");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return response.end("Criação de usuário");
+  }
+
   return response.end("Hello World");
 });
 
